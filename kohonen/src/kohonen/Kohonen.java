974a07de2dc08afadcdc6885503f44e1ff5a523b
Sean <sean.alvarenga@gmail.com>
2013-09-09 16:54:46 -0300

    Estrutura inicial da classe Kohonen

--- kohonen/src/kohonen/Kohonen.java
+++ kohonen/src/kohonen/Kohonen.java
@@ -4,17 +4,43 @@
  */
 package kohonen;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author sean
  */
 public class Kohonen {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        Configuration config = new Configuration();
-        // config.print();
+    
+    private double learningRate;
+    private int radius;
+    private List<Example> trainingSet = new ArrayList<Example>();
+    private List<List<Double>> weightMatrix;
+    
+    public Kohonen() {
+        Configuration configuration = new Configuration();
+        
+        learningRate = configuration.getLearningRate();
+        radius = configuration.getRadius();
+        trainingSet = configuration.getTrainingSet();
+        weightMatrix = configuration.getWeightMatrix();
+    }
+    
+    public void execute() {
+        updateWeight(defineWinner());
+        plot();
+    }
+    
+    public int defineWinner() {
+        return 0;
+    }
+    
+    public void updateWeight(int neuronWinner) {
+        
+    }
+    
+    public void plot() {
+        
     }
 }
