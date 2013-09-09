5a5656b1e102fb367128f85bccfd9b689b07cf0b
Sean <sean.alvarenga@gmail.com>
2013-09-09 19:38:08 -0300

    Testado calculo do neurônio vencedor

--- kohonen/src/kohonen/Kohonen.java
+++ kohonen/src/kohonen/Kohonen.java
@@ -34,14 +34,36 @@ public class Kohonen {
     public void execute() {
         for (Example ex : this.trainingSet) {
             int winner = defineWinner(ex);
-            updateWeight(winner, ex);
+            //updateWeight(winner, ex);
         }
             
-        plot();
+      //  plot(); 
     }
     
     public int defineWinner(Example ex) {
-        return 0;
+        
+        int position = 0;
+        double euclideanDistance = 0;
+        
+        double sum = Math.pow((weightMatrix[0][0] - ex.getBuying()), 2);
+        sum += Math.pow((weightMatrix[1][0] - ex.getMaint()), 2);
+        sum += Math.pow((weightMatrix[2][0] - ex.getSafety()), 2);
+        
+        euclideanDistance = Math.sqrt(sum);
+        
+        int neuronsQuantity = neuronsLine * neuronsColumn; 
+   
+        for (int i = 1; i < neuronsQuantity; i++) {
+            sum = Math.pow((weightMatrix[0][i] - ex.getBuying()), 2);
+            sum += Math.pow((weightMatrix[1][i] - ex.getMaint()), 2);
+            sum += Math.pow((weightMatrix[2][i] - ex.getSafety()), 2);
+            
+            if (Math.sqrt(sum) < euclideanDistance) {
+                euclideanDistance = Math.sqrt(sum);
+                position = i;
+            }
+        }
+        return position;
     }
     
     public void updateWeight(int neuronWinner, Example ex) {
