6422027e62132c821b6e821d5937d36e9a751965
Vinícius Fernandes de Jesus <viniciusffj@gmail.com>
2013-09-10 04:41:58 -0300

    Arrumando os runs sem real time.

--- run/run_20.sh
+++ run/run_20.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "***20 neurônios***";
-java -jar ../kohonen/dist/kohonen.jar -rt=false config_20/tx03pesoigual.properties igual 
-java -jar ../kohonen/dist/kohonen.jar -rt=false config_20/tx03pesorandom.properties random 
-java -jar ../kohonen/dist/kohonen.jar -rt=false config_20/tx075pesoigual.properties igual 
-java -jar ../kohonen/dist/kohonen.jar -rt=false config_20/tx075pesorandom.properties random 
+java -jar ../kohonen/dist/kohonen.jar -rt=false config_20/tx03pesoigual.properties igual &
+java -jar ../kohonen/dist/kohonen.jar -rt=false config_20/tx03pesorandom.properties random &
+java -jar ../kohonen/dist/kohonen.jar -rt=false config_20/tx075pesoigual.properties igual &
+java -jar ../kohonen/dist/kohonen.jar -rt=false config_20/tx075pesorandom.properties random &
 exit 1;
--- run/run_36.sh
+++ run/run_36.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "***36 neurônios***";
-java -jar ../kohonen/dist/kohonen.jar -rt=false config/config2d1.properties
-java -jar ../kohonen/dist/kohonen.jar -rt=false config/config2d2.properties
-java -jar ../kohonen/dist/kohonen.jar -rt=false config/config2e1.properties
-java -jar ../kohonen/dist/kohonen.jar -rt=false config/config2e2.properties
-java -jar ../kohonen/dist/kohonen.jar -rt=false config/config2f1.properties
-java -jar ../kohonen/dist/kohonen.jar -rt=false config/config2f2.properties
+java -jar ../kohonen/dist/kohonen.jar -rt=false config/config2d1.properties &
+java -jar ../kohonen/dist/kohonen.jar -rt=false config/config2d2.properties &
+java -jar ../kohonen/dist/kohonen.jar -rt=false config/config2e1.properties &
+java -jar ../kohonen/dist/kohonen.jar -rt=false config/config2e2.properties &
+java -jar ../kohonen/dist/kohonen.jar -rt=false config/config2f1.properties &
+java -jar ../kohonen/dist/kohonen.jar -rt=false config/config2f2.properties &
 exit 1;
